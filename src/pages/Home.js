53b4119c6fb2015f493a38bcc654fe30d5784ff7
Eninam <megantako@gmail.com>
2020-08-28 19:43:08 -0400

    made the sandbox id private

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,12 @@
 import React, { Component } from 'react';
 import ProductList from '../components/ProductList';
-import {ProductConsumer} from '../context/context'
-
 class home extends Component {
 
     render() {
         return (
             <main>
-                <ProductConsumer>
-                    {
-                        value => {
-                            return <ProductList value={value}/>
-                        }}
-                </ProductConsumer>
 
+                <ProductList />
             </main>
         );
     }
